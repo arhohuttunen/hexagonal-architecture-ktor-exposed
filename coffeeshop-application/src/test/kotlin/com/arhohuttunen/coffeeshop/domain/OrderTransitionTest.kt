915de93b5408739dec104558f79424af8b667962
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-03-08 17:49:29 +0200

    Add tests for the order transitions

---
+++ coffeeshop-application/src/test/kotlin/com/arhohuttunen/coffeeshop/domain/OrderTransitionTest.kt
@@ -0,0 +1,67 @@
+package com.arhohuttunen.coffeeshop.domain
+
+import com.arhohuttunen.coffeeshop.domain.OrderTestFactory.aPaidOrder
+import com.arhohuttunen.coffeeshop.domain.OrderTestFactory.anOrder
+import com.arhohuttunen.coffeeshop.domain.OrderTestFactory.anOrderInPreparation
+import com.arhohuttunen.coffeeshop.domain.OrderTestFactory.aReadyOrder
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+class OrderTransitionTest : FunSpec({
+    test("updating a placed order changes location and items") {
+        val order = anOrder()
+        val newItems = listOf(LineItem(Drink.ESPRESSO, Milk.SKIMMED, Size.SMALL, 2))
+
+        val updated = order.update(Location.IN_STORE, newItems)
+
+        updated.id shouldBe order.id
+        updated.location shouldBe Location.IN_STORE
+        updated.items shouldContainExactly newItems
+    }
+
+    test("paying a placed order produces a paid order") {
+        val order = anOrder()
+
+        val paid = order.pay()
+
+        paid.shouldBeInstanceOf<Order.Paid>()
+        paid.id shouldBe order.id
+        paid.location shouldBe order.location
+        paid.items shouldContainExactly order.items
+    }
+
+    test("starting preparation of a paid order produces an order in preparation") {
+        val order = aPaidOrder()
+
+        val inPreparation = order.startPreparing()
+
+        inPreparation.shouldBeInstanceOf<Order.InPreparation>()
+        inPreparation.id shouldBe order.id
+        inPreparation.location shouldBe order.location
+        inPreparation.items shouldContainExactly order.items
+    }
+
+    test("finishing preparation produces a ready order") {
+        val order = anOrderInPreparation()
+
+        val ready = order.finishPreparing()
+
+        ready.shouldBeInstanceOf<Order.Ready>()
+        ready.id shouldBe order.id
+        ready.location shouldBe order.location
+        ready.items shouldContainExactly order.items
+    }
+
+    test("taking a ready order produces a taken order") {
+        val order = aReadyOrder()
+
+        val taken = order.take()
+
+        taken.shouldBeInstanceOf<Order.Taken>()
+        taken.id shouldBe order.id
+        taken.location shouldBe order.location
+        taken.items shouldContainExactly order.items
+    }
+})
