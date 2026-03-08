afc4684bf676e59648a206f3f104a984871cf07d
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-03-08 19:52:24 +0200

    Make the test a little more readable

--- coffeeshop-application/src/test/kotlin/com/arhohuttunen/coffeeshop/domain/OrderCostTest.kt
+++ coffeeshop-application/src/test/kotlin/com/arhohuttunen/coffeeshop/domain/OrderCostTest.kt
@@ -6,25 +6,45 @@ import java.math.BigDecimal
 
 class OrderCostTest : FunSpec({
     test("1 small drink costs 4.00") {
-        val order = anOrder { items(aLineItem { size = Size.SMALL }) }
+        val order = anOrder {
+            items(
+                aLineItem { size = Size.SMALL }
+            )
+        }
 
         order.cost() shouldBe BigDecimal("4.00")
     }
 
     test("1 large drink costs 5.00") {
-        val order = anOrder { items(aLineItem { size = Size.LARGE }) }
+        val order = anOrder {
+            items(
+                aLineItem { size = Size.LARGE }
+            )
+        }
 
         order.cost() shouldBe BigDecimal("5.00")
     }
 
     test("2 small drinks costs 8.00") {
-        val order = anOrder { items(aLineItem { size = Size.SMALL; quantity = 2 }) }
+        val order = anOrder {
+            items(
+                aLineItem {
+                    size = Size.SMALL
+                    quantity = 2
+                }
+            )
+        }
 
         order.cost() shouldBe BigDecimal("8.00")
     }
 
     test("1 large and 1 small drinks costs 9.00") {
-        val order = anOrder { items(aLineItem { size = Size.LARGE }, aLineItem { size = Size.SMALL }) }
+        val order = anOrder {
+            items(
+                aLineItem { size = Size.LARGE },
+                aLineItem { size = Size.SMALL }
+            )
+        }
 
         order.cost() shouldBe BigDecimal("9.00")
     }
