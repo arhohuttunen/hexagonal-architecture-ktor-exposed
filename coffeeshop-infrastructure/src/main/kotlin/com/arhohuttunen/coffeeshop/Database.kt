d549f7c3ccf426e13fc39e7382a5af5a52233e4a
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-03-08 11:57:51 +0200

    Create schemas on startup

--- coffeeshop-infrastructure/src/main/kotlin/com/arhohuttunen/coffeeshop/Database.kt
+++ coffeeshop-infrastructure/src/main/kotlin/com/arhohuttunen/coffeeshop/Database.kt
@@ -1,8 +1,13 @@
 package com.arhohuttunen.coffeeshop
 
+import com.arhohuttunen.coffeeshop.adapter.outbound.persistence.OrderItemsTable
+import com.arhohuttunen.coffeeshop.adapter.outbound.persistence.OrdersTable
+import com.arhohuttunen.coffeeshop.adapter.outbound.persistence.PaymentsTable
 import io.ktor.server.application.*
 import io.ktor.server.config.*
 import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 fun Application.configureDatabase(config: ApplicationConfig) {
     Database.connect(
@@ -11,4 +16,8 @@ fun Application.configureDatabase(config: ApplicationConfig) {
         user = config.property("database.user").getString(),
         password = config.property("database.password").getString(),
     )
+
+    transaction {
+        SchemaUtils.create(OrdersTable, OrderItemsTable, PaymentsTable)
+    }
 }
