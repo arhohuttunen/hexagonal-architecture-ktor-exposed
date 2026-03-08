96a0529fb767373f3ce80f7c1e16ed6fbef695bf
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-03-08 10:41:13 +0200

    Implement finding an order

--- coffeeshop-infrastructure/src/main/kotlin/com/arhohuttunen/coffeeshop/adapter/outbound/persistence/ExposedOrdersRepository.kt
+++ coffeeshop-infrastructure/src/main/kotlin/com/arhohuttunen/coffeeshop/adapter/outbound/persistence/ExposedOrdersRepository.kt
@@ -1,10 +1,14 @@
 package com.arhohuttunen.coffeeshop.adapter.outbound.persistence
 
+import com.arhohuttunen.coffeeshop.application.ports.outbound.OrderNotFound
 import com.arhohuttunen.coffeeshop.application.ports.outbound.Orders
+import com.arhohuttunen.coffeeshop.domain.LineItem
 import com.arhohuttunen.coffeeshop.domain.Order
+import org.jetbrains.exposed.v1.core.ResultRow
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.batchInsert
 import org.jetbrains.exposed.v1.jdbc.deleteWhere
+import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.jetbrains.exposed.v1.jdbc.upsert
 import kotlin.uuid.Uuid
@@ -31,10 +35,33 @@ object ExposedOrdersRepository : Orders {
     }
 
     override fun findById(orderId: Uuid): Order {
-        TODO("Not yet implemented")
+        return transaction {
+            val orderRow = OrdersTable.selectAll()
+                .where { OrdersTable.id eq orderId }
+                .singleOrNull() ?: throw OrderNotFound()
+
+            val items = OrderItemsTable.selectAll()
+                .where { OrderItemsTable.orderId eq orderId }
+                .map { it.toLineItem() }
+
+            orderRow.toOrder(items)
+        }
     }
 
     override fun deleteById(orderId: Uuid) {
         TODO("Not yet implemented")
     }
 }
+
+private fun ResultRow.toLineItem() = LineItem(
+    drink = this[OrderItemsTable.drink],
+    milk = this[OrderItemsTable.milk],
+    size = this[OrderItemsTable.size],
+    quantity = this[OrderItemsTable.quantity],
+)
+
+private fun ResultRow.toOrder(items: List<LineItem>) = Order(
+    id = this[OrdersTable.id],
+    location = this[OrdersTable.location],
+    items = items
+)
--- coffeeshop-infrastructure/src/test/kotlin/com/arhohuttunen/coffeeshop/adapter/outbound/persistence/ExposedOrdersRepositoryTest.kt
+++ coffeeshop-infrastructure/src/test/kotlin/com/arhohuttunen/coffeeshop/adapter/outbound/persistence/ExposedOrdersRepositoryTest.kt
@@ -1,11 +1,13 @@
 package com.arhohuttunen.coffeeshop.adapter.outbound.persistence
 
+import com.arhohuttunen.coffeeshop.application.ports.outbound.OrderNotFound
 import com.arhohuttunen.coffeeshop.domain.Drink
 import com.arhohuttunen.coffeeshop.domain.LineItem
 import com.arhohuttunen.coffeeshop.domain.Location
 import com.arhohuttunen.coffeeshop.domain.Milk
 import com.arhohuttunen.coffeeshop.domain.Order
 import com.arhohuttunen.coffeeshop.domain.Size
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.testcontainers.TestContainerProjectExtension
@@ -15,6 +17,7 @@ import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.testcontainers.containers.PostgreSQLContainer
+import kotlin.uuid.Uuid
 
 class ExposedOrdersRepositoryTest : FunSpec({
     val postgres = install(TestContainerProjectExtension(PostgreSQLContainer<Nothing>("postgres")))
@@ -43,4 +46,31 @@ class ExposedOrdersRepositoryTest : FunSpec({
         persistedOrder.location shouldBe Location.TAKE_AWAY
         persistedOrder.items shouldContainExactly listOf(LineItem(Drink.LATTE, Milk.WHOLE, Size.SMALL, 1))
     }
+
+    test("finding a placed order returns its details") {
+        val orderId = havingPersisted(
+            Order(
+                location = Location.IN_STORE,
+                items = listOf(
+                    LineItem(Drink.ESPRESSO, Milk.SKIMMED, Size.LARGE, 1)
+                )
+            )
+        )
+
+        val order = ExposedOrdersRepository.findById(orderId)
+
+        order.location shouldBe Location.IN_STORE
+        order.items shouldContainExactly listOf(LineItem(Drink.ESPRESSO, Milk.SKIMMED, Size.LARGE, 1))
+    }
+
+    test("finding a non-existing order throws an exception") {
+        shouldThrow<OrderNotFound> {
+            ExposedOrdersRepository.findById(Uuid.random())
+        }
+    }
 })
+
+fun havingPersisted(order: Order): Uuid {
+    ExposedOrdersRepository.save(order)
+    return order.id
+}
