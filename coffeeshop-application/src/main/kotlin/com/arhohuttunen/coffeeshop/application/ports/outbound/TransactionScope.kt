8e01897641ca68ba14a6441b25f8bd5555e71cab
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-03-15 16:01:08 +0200

    Make the transaction scope slightly more readable

--- coffeeshop-application/src/main/kotlin/com/arhohuttunen/coffeeshop/application/ports/outbound/TransactionScope.kt
+++ coffeeshop-application/src/main/kotlin/com/arhohuttunen/coffeeshop/application/ports/outbound/TransactionScope.kt
@@ -4,7 +4,9 @@ interface TransactionScope {
     fun <T> execute(block: () -> T): T
 
     companion object {
-        operator fun invoke(): TransactionScope = object : TransactionScope {
+        operator fun invoke(): TransactionScope = NoOp()
+
+        private class NoOp : TransactionScope {
             override fun <T> execute(block: () -> T): T = block()
         }
     }
